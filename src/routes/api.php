f8edf832effc1a8b9fc265faee98197ed42bbea9
m.kiselev <mike.kiselev@gmail.com>
2020-10-08 22:47:42 +0300

    route

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\V1\TagController;
+use \App\Http\Controllers\Api\V1\PostController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('/tags', TagController::class)->only(['index', 'show', 'store', 'update']);
+
+Route::apiResource('/posts', PostController::class);
